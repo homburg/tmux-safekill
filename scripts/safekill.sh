2878bfebb64890882b302f010ba35c16cc96abeb
Thomas B Homburg <thomas@madethis.dk>
2020-06-10 23:15:19 +0200

    Tweak to kill mitmproxy

--- scripts/safekill.sh
+++ scripts/safekill.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# non-interactive shell won't source in ~/.bashrc by default
+# non-interactive shell won't source in ~/.bashrc by default"}
 # use our own bash configuration
 if [ -f ~/.bashrc_tmux_safekill ]; then
   source ~/.bashrc_tmux_safekill
@@ -19,7 +19,7 @@ function read_env_config {
   fi
 }
 
-function safe_end_procs {
+function safe_end_procs() {
   old_ifs="$IFS"
   IFS=$'\n'
 
@@ -31,6 +31,8 @@ function safe_end_procs {
       cmd=$([[ $envcfg_use_forcequit_approach == 1 ]] && echo 'Escape ":qa!" Enter' || echo 'Escape ":qa" Enter')
     elif [[ "$pane_proc" == "man" ]] || [[ "$pane_proc" == "less" ]]; then
       cmd='"q"'
+    elif [[ "$pane_proc" == "python" ]]; then # mitmproxy
+      cmd='C-c "y"'
     elif [[ "$pane_proc" == "bash" ]] || [[ "$pane_proc" == "zsh" ]] || [[ "$pane_proc" == "fish" ]]; then
       cmd='C-c C-u space "exit" Enter'
     elif [[ "$pane_proc" == "ssh" ]] || [[ "$pane_proc" == "vagrant" ]]; then
@@ -43,7 +45,7 @@ function safe_end_procs {
   IFS="$old_ifs"
 }
 
-function safe_kill_panes_of_current_session {
+function safe_kill_panes_of_current_session() {
   session_name=$(tmux display-message -p '#S')
   current_panes=$(tmux list-panes -a -F "#{pane_id} #{pane_current_command} #{session_name}\n" | grep "$session_name")
 
@@ -52,14 +54,14 @@ function safe_kill_panes_of_current_session {
   array=($current_panes)
   # Restore IFS
   IFS=$SAVEIFS
-  for (( i=0; i<${#array[@]}; i++ ))
-  do
+  for ((i = 0; i < ${#array[@]}; i++)); do
     safe_end_procs "${array[$i]}"
     sleep 0.8
   done
 }
 
 read_env_config
+safe_kill_panes_of_current_session
 safe_kill_panes_of_current_session
 safe_kill_panes_of_current_session
 exit 0
